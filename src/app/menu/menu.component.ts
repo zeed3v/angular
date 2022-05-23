4212236de0cc90641ff97e1e3480c3b85c25c2df
zeed3v <felipebrionese@gmail.com>
2022-05-23 03:40:48 -0400

    assignment 2 angular

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
