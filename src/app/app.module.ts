5635adabe326628ed8bccf2e821cc81963eeb6fe
zeed3v <felipebrionese@gmail.com>
2022-05-23 00:09:28 -0400

    angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,20 @@ import { MatGridListModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { FooterComponent } from './footer/footer.component'
     MenuComponent,
     DishdetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
